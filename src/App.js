47e7511394bd21f6a5fa9858934cc997f88ecc29
NiklasReents <61356983+NiklasReents@users.noreply.github.com>
2020-04-10 16:54:53 +0200

    delete button fully functional + code clean-up
    removed redundant "Main.js" + components folder, delete button finished, minor changes to props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-import RecipeStyle from "./RecipeStyle"
+import Recipestyle from "./Recipestyle";
 
 const fetchData = (recipeURL, setRecipes) => {
   fetch(recipeURL)
     .then(result => result.json())
     .then(finalResult => setRecipes(finalResult))
-
-
     .catch(error => alert("Error", error));
-  //move to a separate "helpers" file (keep functions out of the APP component)?
-  //a promise is an js object, that works with asynchronous code
-  //which may be pending, successfully fetched (you get the result) or it can fail (which error catches account for)
-
-
 };
 
 export default function App() {
-  const recipeURL = "https://api.spoonacular.com/recipes/random?apiKey=7bd8808bacc941f5b6d18386bdfb02ad&number=3&number=3&tag=vegan";
+  const recipeURL =
+    "https://api.spoonacular.com/recipes/random?apiKey=96c75f90d9c9441286b0d206d58551c0&number=3&number=3&tag=vegan";
   const [recipes, setRecipes] = useState();
+  const [deleteRecipe, setDeleteRecipe] = useState();
 
-  function deleteFunction(index) {
-    setRecipes(recipes.recipes.filter((element, filterIndex) => filterIndex !== index));
-  }
-
-    
+  const deleteFunction = index => {
+    setDeleteRecipe(recipes.recipes.splice(index, 1));
+  };
 
   useEffect(() => {
     fetchData(recipeURL, setRecipes);
@@ -34,18 +27,15 @@ export default function App() {
   const recipeItems = recipes
     ? recipes.recipes.map((element, index) => {
         return (
-
           <div>
-            <RecipeStyle 
-            key={index}
-            title={element.title}
-            recipeURL={element.sourceUrl}
-            imgSrc={element.image}
-            deletR={() => deleteFunction(index)}
-            creditsText={element.creditsText}
-          />
-
-
+            <Recipestyle
+              index={index}
+              title={element.title}
+              recipeURL={element.sourceUrl}
+              imgSrc={element.image}
+              deleter={deleteFunction}
+              creditsText={element.creditsText}
+            />
           </div>
         );
       })
@@ -53,20 +43,16 @@ export default function App() {
 
   return (
     <div className="App">
-
       <nav class="navbar navbar-light bg-light">
         <span class="navbar-brand mb-0 h1">Recipe List</span>
-        <button class="btn btn-outline-success my-2 my-sm-0" onClick={() => fetchData(recipeURL, setRecipes)}>Fetch Data</button>
+        <button
+          class="btn btn-outline-success my-2 my-sm-0"
+          onClick={() => fetchData(recipeURL, setRecipes)}
+        >
+          Fetch Data
+        </button>
       </nav>
-  
-      <br />{" "}
-      <div class="recipeWrapper">{recipeItems}</div>
-
+      <br /> <div class="recipeWrapper">{recipeItems}</div>
     </div>
   );
 }
-/*setRecipes((recipes.recipes = ""))
-() => console.log(recipes.recipes[index])
-
-*/
-
