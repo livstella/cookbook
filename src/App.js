390a2ded2571b03cd57d2b8d22c3b898521aca09
Liv.R <livreinhold@gmail.com>
2020-04-10 12:21:50 +0200

    With bootstrap layout

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 import RecipeStyle from "./RecipeStyle"
 
-const fetchData = (recipeURL, setRecipes, setError) => {
+const fetchData = (recipeURL, setRecipes) => {
   fetch(recipeURL)
     .then(result => result.json())
     .then(finalResult => setRecipes(finalResult))
-    .catch(error => setError(error));
+    .catch(error => console.log(error));
 
 };
 
 export default function App() {
-  const recipeURL = "https://api.spoonacular.com/recipes/random?apiKey=4075a91c4cec4364a6a65681a3cac3dd&number=6&number=3&tag=vegan";
+  const recipeURL = "https://api.spoonacular.com/recipes/random?apiKey=4075a91c4cec4364a6a65681a3cac3dd&number=3&number=3&tag=vegan";
   const [recipes, setRecipes] = useState();
-  const [error, setError] = useState();
+
+  function deleteFunction(index) {
+    setRecipes(recipes.recipes.filter((element, filterIndex) => filterIndex !== index));
+  }
     
   useEffect(() => {
-    fetchData(recipeURL, setRecipes, setError);
+    fetchData(recipeURL, setRecipes);
   }, []);
 
   const recipeItems = recipes
@@ -24,14 +27,12 @@ export default function App() {
         return (
           <div>
             <RecipeStyle 
+            key={index}
             title={element.title}
             recipeURL={element.sourceUrl}
             imgSrc={element.image}
+            deletR={deleteFunction}
           />
-          <button 
-            onClick={() => console.log(recipes.recipes[index])}> 
-            Delete a recipe
-        </button>
           </div>
         );
       })
@@ -40,14 +41,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Recipe List</h1>
-      <button onClick={() => fetchData(recipeURL, setRecipes, setError)}>
+      <button onClick={() => fetchData(recipeURL, setRecipes)}>
         Fetch Data
       </button>
       <br />{" "}
-      <div>{recipeItems}</div>
-      <div>{error}</div>
+      <div class="recipeWrapper">{recipeItems}</div>
+
     </div>
   );
 }
-/*setRecipes((recipes.recipes = ""))}
+/*setRecipes((recipes.recipes = ""))
+() => console.log(recipes.recipes[index])
+
 */
--- src/Recipestyle.js
+++ src/Recipestyle.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function RecipeStyle(props) {
-  const { title, recipeUrl, imgSrc } = props;
+  const { title, recipeUrl, imgSrc, deleteR } = props;
   return (
-    <div>
-      <h2> {title} </h2>
-      <img src={imgSrc} alt="food img" />
-  <p><a href={recipeUrl}>Go to the recipe for {title}</a></p>
+    <div class="card">
+        <img src={imgSrc} alt="food img" class="card-img-top"/>
+          <div class="card-body">
+            <h2 class="card-title"> {title} </h2>
+            <p><a href={recipeUrl}>Go to the recipe for {title}</a></p>
+            <button 
+              onClick={deleteR}> 
+              Delete a recipe
+          </button>
+      </div>
     </div>
   );
 }
