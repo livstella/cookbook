0788e7a1a655e9a03acc046137fbc086b8d5ff96
NiklasReents <Niklas.Reents@gmail.com>
2020-04-29 23:22:19 +0200

    add category filter for recipe components; add map function for scaleable recipe ingredients- and measurements-tables

--- src/recipe_components/Breakfast.js
+++ src/recipe_components/Breakfast.js
@@ -7,7 +7,7 @@ export default function Desserts() {
 
   useEffect(() => {
     client
-      .getEntries({ content_type: "meals" })
+      .getEntries({ content_type: "meals", 'fields.category[match]': 'Breakfast' })
       .then(entries => {
         setData(entries);
       })
